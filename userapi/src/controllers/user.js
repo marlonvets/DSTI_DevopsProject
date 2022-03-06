af800b74c3ebddd4b4e201ed47def90ce57e5e47
Marlon Thompson <mmthompson1011@gmail.com>
2022-03-06 16:34:09 -0500

    working set and get with checks for username existence

--- userapi/src/controllers/user.js
+++ userapi/src/controllers/user.js
@@ -12,32 +12,54 @@ module.exports = {
     }
     // Save to DB
     // TODO check if user already exists
-    client.hmset(user.username, userObj, (err, res) => {
-      if (err) return callback(err, null)
-      callback(null, res) // Return callback
+        client.exists(user.username, function (err, reply) {
+      if (reply === 0) {
+                client.hmset(user.username, userObj, (err, res) => {
+                    if (err) return callback(err, null)
+                    callback(null, res) // Return callback
+                })
+
+      } else {
+                console.log('user exists!');
+                const object = "user by that name"
+
+                return callback(null, object)
+      }
     })
   },
 	get: (username, callback) => {
+        if (!username)
+            return callback(new Error("username parameter required!"), null)
+        client.exists(username, function (err, reply) {
+           if (reply === 1) {
+              client.hgetall(username, function (err, object) {
+                if (err) return callback(err, null)
+                console.log(object)
+                  console.log("get sec,  Exists!");
+                return callback(null, object)
+              })
+           } else {
+               console.log('Doesn\'t exist!');
+               const object =  "no user by that name"
+                       
+               return callback(null, object)
+           }
+            });
 
-	client.hgetall(username, function(err, object) {
-  console.log(object)
-   console.log("get sec");
-    return callback(null, object)
-
-});
-  },
+},
 	update: (user, callback) => {
 	
-  const userObj = {
+        const userObj = {
       firstname: user.firstname,
       lastname: user.lastname,
-    }
-	client.put(user.username, userObj, (err, res) => {
+        }
+	    client.put(user.username, userObj, (err, res) => {
       if (err) return callback(err, null)
       callback(null, res) // Return callback
     })
 
-});
+}
 }
 
-}
+
+ 

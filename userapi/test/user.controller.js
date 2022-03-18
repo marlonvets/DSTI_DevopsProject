f6671901346b504a1e9e1b87ddaf01c5a6b1a4cd
Marlon Thompson <mmthompson1011@gmail.com>
2022-03-18 00:04:08 -0500

    UPDATE: Added tests for GET and UPDATE functions TO DO: Add tests for delete function

--- userapi/test/user.controller.js
+++ userapi/test/user.controller.js
@@ -57,31 +57,115 @@ describe('User', () => {
                 done()
             })
         })
-  /*  describe('Get', ()=> {
+    describe('Get', ()=> {
         //   // TODO Create test for the get method
         it('get a user by username', (done) => {
           const user = {
-                username: 'bob',
-                firstname: 'Robert',
-                lastname: 'Marley'
+                username: 'teacha',
+                firstname: 'Vybz',
+                lastname: 'Kartel'
           }
+
           userController.create(user, (err, result) => {
-                expect(err).to.be.equal(null)
-                expect(result).to.be.equal('OK')
-                 
-          })
-            const username='bob'
-            userController.get(username, (err, result) => {
-                expect(err).to.be.equal(null)
-                expect(result).to.be.equal(user)
+   
+              const username = user.username
+          userController.get(username, (err, result) => {
+              expect(err).to.be.equal(null)
+              console.log(result);
+              expect(result.firstname).to.be.equal(user.firstname)
+              expect(result.lastname).to.be.equal(user.lastname)
                 done()
-           })
-
-        //     // 1. First, create a user to make this unit test independent from the others
-        //     // 2. Then, check if the result of the get method is correct
-             
-
           })
-         })*/
+          })
+
+        })
+
+        it('get a user that doesn\'t exist', (done) => {
+                 const username = 'poppy'
+           userController.get(username, (err, result) => {
+            expect(err).to.not.be.equal(null)
+            console.log(result);
+               expect(result).to.be.equal(null)
+             done()
+                
+            })
+        })
+
+        it('get a user with no parameter', (done) => {
+            const username = null
+            userController.get(username, (err, result) => {
+                expect(err).to.not.be.equal(null)
+                console.log(result);
+                expect(result).to.be.equal(null)
+                done()
+
+            })
+        })
+
+
+    })
+
+        describe('Update', () => {
+
+            it('clear db', (done) => {
+                userController.clear('username', (err, result) => {
+                    expect(err).to.be.equal(null)
+                    //       expect(result).to.be.equal('OK')
+                    done()
+                })
+
+            })
+
+            it('update a user that doesn\'t exist', (done) => {
+                const user = {
+                    username: 'teacha',
+                    firstname: 'Vybz',
+                    lastname: 'Kartel'
+                }
+
+                userController.update(user, (err, result) => {
+                    expect(err).to.not.be.equal(null)
+                    console.log(result);
+                    expect(result).to.be.equal(null)
+                    done()
+
+                })
+            })
+
+           it('passing wrong user parameters', (done) => {
+                const user = {
+                    firstname: 'Robert',
+                    lastname: 'Marley'
+                }
+                userController.update(user, (err, result) => {
+                    expect(err).to.not.be.equal(null)
+                    expect(result).to.be.equal(null)
+                    done()
+                })
+            })
+
+            it('update an existing user', (done) => {
+               const user = {
+                    username: 'teacha',
+                    firstname: 'Vybz',
+                    lastname: 'Kartel'
+                }
+
+                userController.create(user, (err, result) => {
+                    userController.update(user, (err, result) => {
+                        expect(err).to.be.equal(null)
+                        console.log(result);
+                     //   expect(result.firstname).to.be.equal(user.firstname)
+                     //   expect(result.lastname).to.be.equal(user.lastname)
+                        done()
+                    })
+                })
+            })
+
+
+
+
+
+    })
     })
 })

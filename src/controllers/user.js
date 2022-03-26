7b5e5a1372c8ffea5a157416bbbbd0824a3f14fb
Marlon Thompson <mmthompson1011@gmail.com>
2022-03-26 12:53:53 -0500

    update files

---
+++ src/controllers/user.js
@@ -0,0 +1,113 @@
+const client = require('../dbClient')
+
+module.exports = {
+  create: (user, callback) => {
+    // Check parameters
+    if(!user.username)
+      return callback(new Error("Wrong user parameters"), null)
+    // Create User schema
+    const userObj = {
+      firstname: user.firstname,
+      lastname: user.lastname,
+    }
+    // Save to DB
+    // TODO check if user already exists
+        client.exists(user.username, function (err, reply) {
+      if (reply === 0) {
+                client.hmset(user.username, userObj, (err, res) => {
+                    if (err) return callback(err, null)
+                    callback(null, res) // Return callback
+                })
+
+      } else {
+        //        console.log('user exists!');
+          return callback(new Error("user already exists!"), null)
+      }
+    })
+  },
+	get: (username, callback) => {
+        if (!username)
+            return callback(new Error("username parameter required!"), null)
+        client.exists(username, function (err, reply) {
+           if (reply === 1) {
+              client.hgetall(username, function (err, object) {
+                if (err) return callback(err, null)
+                console.log(object)
+                  console.log("get sec,  Exists!");
+                return callback(null, object)
+              })
+           } else {
+               console.log('Doesn\'t exist!');
+           //    const object =  "no user by that name"
+               return callback(new Error("user doesn't exist!"), null)            
+            //   return callback(null, object)
+           }
+            });
+
+},
+    update: (user, callback) => {
+        // Check parameters
+        if (!user.username)
+            return callback(new Error("Wrong user parameters"), null)
+        // Create User schema
+        const userObj = {
+            firstname: user.firstname,
+            lastname: user.lastname,
+        }
+        // Save to DB
+        // TODO check if user already exists
+        client.exists(user.username, function (err, reply) {
+            if (reply === 1) {
+                client.hmset(user.username, userObj, (err, res) => {
+                    if (err) return callback(err, null)
+                    callback(null, res) // Return callback
+                })
+
+            } else {
+                console.log('user exists!');
+                return callback(new Error("user does not exists!"), null)
+            }
+        })
+    },
+
+    delete: (user, callback) => {
+        if (!user.username)
+            return callback(new Error("username parameter required!"), null)
+        client.exists(user.username, function (err, reply) {
+            if (reply === 1) {
+                console.log(' exist!')
+                
+                client.del(user.username, function (err, object) {
+                   if (err) return callback(err, null)
+                    object = "user deleted: " + user.username
+                   console.log(object)
+                return callback(null, object)
+                })
+       
+            } else {
+                console.log('Doesn\'t exist!');
+                //    const object =  "no user by that name"
+                return callback(new Error("user doesn't exist!"), null)
+                //   return callback(null, object)
+            }
+        
+        });
+
+    },
+
+    clear: (username, callback) => {
+        client.flushdb(function (err, succeeded) {
+            console.log(succeeded); // will be true if successfull
+            return callback(null, succeeded)
+        });
+        
+    }  
+ 
+
+    }
+
+
+ 
+
+
+ 
---
+++ src/routes/user.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const userController = require('../controllers/user')
+
+const userRouter = express.Router()
+
+userRouter
+  .post('/', (req, resp) => {
+    userController.create(req.body, (err, res) => {
+      let respObj
+      if(err) {
+        respObj = {
+          status: "error",
+          msg: err.message
+        }
+        return resp.status(400).json(respObj)
+      }
+      respObj = {
+        status: "success",
+        msg: res
+      }
+      resp.status(201).json(respObj)
+    })
+		console.log(req.body); 
+  })
+ //   .get('/', (req, resp, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
+   .get('/:username', (req, resp, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
+  //   // TODO Create get method API
+     const username = req.params.username
+	  userController.get(username, (err, res) => {
+      let respObj
+      if(err) {
+        respObj = {
+          status: "error",
+          msg: err.message
+        }
+        return resp.status(400).json(respObj)
+      }
+      
+      respObj = {
+        status: "success",
+        msg: res
+      }
+      resp.status(201).json(respObj)
+    })
+		 
+  })
+    .put('/', (req, resp) => {
+    userController.update(req.body, (err, res) => {
+      let respObj
+      if(err) {
+        respObj = {
+          status: "error",
+          msg: err.message
+        }
+        return resp.status(400).json(respObj)
+      }
+      respObj = {
+        status: "success",
+        msg: res
+      }
+      resp.status(201).json(respObj)
+    })
+		console.log(req.body); 
+    })
+
+    .delete('/', (req, resp, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
+        //   // TODO Create get method API
+      //  const username = req.body,
+            userController.delete(req.body, (err, res) => {
+            let respObj
+            if (err) {
+                respObj = {
+                    status: "error",
+                    msg: err.message
+                }
+                return resp.status(400).json(respObj)
+            }
+            
+               respObj = {
+                status: "success",
+                   msg: res
+               }
+             
+            resp.status(201).json(respObj)
+        })
+    })
+
+
+  /*  .purge('/', (req, resp, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
+        //   // TODO Create get method API
+      //  const username = req.body,
+        client.flushall('ASYNC', callback);
+    })*/
+module.exports = userRouter
